aad9023b462035eb00b5a27a286332a345b7a3d6
hieun33 <grace3029@gmail.com>
2022-11-11 10:28:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './scss/style.scss';
+import { Canvas, extend, useFrame } from 'react-three-fiber';
+import { useRef } from 'react';
+
+
+const Box = () => {
+	useFrame((state)=>{			//useFrame 화면의주사율에 맞게 1초마다 재랜더링해주는아이
+		console.log(state);
+	})
+	return(
+		<mesh>
+			<boxBufferGeometry />
+			<meshBasicMaterial color='blue' />
+		</mesh>
+
+	)
+}
 
 function App() {
-
 	return (
 		<figure>
-
+			<Canvas				
+				style={{ background: '#111' }}
+				// x,y,z 축
+				camera={{ position: [7, 7, 7] }}>
+											
+				<axesHelper args={[6]} />
+				<Box />
+			</Canvas>
 		</figure>
 	);
 }
