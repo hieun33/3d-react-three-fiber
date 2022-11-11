c039614b8093139675eaad439e87978023922dd4
hieun33 <grace3029@gmail.com>
2022-11-11 14:34:38 +0900

    색상컴포넌트와 본래값복귀 추가

--- src/App.js
+++ src/App.js
@@ -70,8 +70,21 @@ const Floor = (props) => {
 	)
 }
 function App() {
+	const handleClick = e => {
+		//색상 패널 클릭시 만약 전역에 등록한 오브제가 없으면 종료
+		if (!window.activeMesh) return;
+		window.activeMesh.material.color = new THREE.Color(e.target.style.background);
+	}
 	return (
 		<figure>
+
+<article className="colorPicker">
+				<div style={{ background: 'blue' }} onClick={handleClick}></div>
+				<div style={{ background: 'red' }} onClick={handleClick}></div>
+				<div style={{ background: 'green' }} onClick={handleClick}></div>
+				<div style={{ background: 'transparent' }} onClick={handleClick}></div>
+			</article>
+
 			<Canvas
 				//그림자 설정시 필요함
 				shadowMap
@@ -79,6 +92,7 @@ function App() {
 				camera={{ position: [3, 5, 3] }}>
 				<axesHelper args={[6]} />
 				<Orbit />
+
 				<ambientLight intensity={0.2} />
 				<Bulb position={[0, 3, 0]} />
 				<Suspense fallback={null}>
